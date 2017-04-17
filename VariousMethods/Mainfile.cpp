d28be2bfa0b8ff87fb1990cf6d24393f85a5f153
NoahNMorton <noahmorton98@gmail.com>
2017-04-17 14:05:00 -0500

    Started work on VariousMethods lab.

---
+++ VariousMethods/Mainfile.cpp
@@ -0,0 +1,36 @@
+/*
+ * Mainfile.cpp
+ *
+ *  Created on: Apr 17, 2017
+ *      Author: othscs253
+ */
+
+#include <string>
+#include "Mainfile.h"
+
+
+int main() {
+
+	return 0;
+}
+
+
+std::string numberRange(int lower, int upper) {
+	return "";
+}
+
+std::string letterRange(char lower, char upper) {
+	return "";
+}
+
+int timeToMins(int hours, int minutes) {
+	return 0;
+}
+
+std::string intToString(int integer) {
+	return "";
+}
+
+void favorites() {
+
+}
---
+++ VariousMethods/Mainfile.h
@@ -0,0 +1,24 @@
+/*
+ * Mainfile.h
+ *
+ *  Created on: Apr 17, 2017
+ *      Author: othscs253
+ */
+
+#ifndef MAINFILE_H_
+#define MAINFILE_H_
+
+#include <string>
+
+std::string numberRange(int lower, int upper);
+
+std::string letterRange(char lower, char upper);
+
+int timeToMins(int hours, int minutes);
+
+std::string intToString(int integer);
+
+void favorites();
+
+
+#endif /* MAINFILE_H_ */
